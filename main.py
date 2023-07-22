6ce26326fb9333a25089b23f9ce472e32a1ff087
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-07-22 23:42:10 +0200

    finale version

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
 from docx import Document
-from docx.enum.style import WD_STYLE_TYPE
-from docx.oxml.ns import nsdecls
-from docx.oxml import parse_xml
 import re
 import datetime
 import locale
 
 # Path to the Word document
-document_path = r"" # Path to your document
-changed_document_path = r"" # Path to the changed document
+document_path = r"E:/Protokoll 2023.docx"  # Path to your document
+changed_document_path = r"E:/Protokoll 2023_Wochentage3.docx"  # Path to the changed document
 # Create a new document instance
 document = Document(document_path)
 
-# Get the 'Heading 3' style
-style_heading3 = document.styles['Heading 3 Char']
-
-# Modify the 'Heading 3' style to hide the numbering
-xml = '''
-        <w:pPr xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
-            <w:numPr>
-                <w:ilvl w:val="0"/>
-                <w:numId w:val="0"/>
-            </w:numPr>
-        </w:pPr>
-      '''
-xml_element = parse_xml(xml)
-style_heading3._element.append(xml_element)
-
-pattern = r"\d{2}\.\d{2}:"
+pattern = r"\d{2}\.\d{2}(?:\.\d{2})?"
 
 def weekday(datum):
-    day, month = map(int, datum.split('.'))
+    day, month, *_ = map(int, datum.split('.'))
     year = datetime.datetime.now().year  # uses the current year
 
     locale.setlocale(locale.LC_TIME, 'de_DE.UTF-8')
@@ -40,23 +22,24 @@ def weekday(datum):
 
     return weekday
 
+def enhance_date_with_weekday(text):
+    matches = re.findall(pattern, text)
+    for match in matches:
+        date = match  # Remove the trailing colon
+        weekday_text = f"{date} ({weekday(date)}):"
+        # Replace the date pattern with the formatted text
+        text = text.replace(match, weekday_text)
+    return text
+
 def main():
-
     for paragraph in document.paragraphs:
-        match = re.findall(pattern, paragraph.text)
-        print(match)
-        if match:
-            date = ''.join(match)[:-1]
-            print(date)
-            paragraph.text = f""
-            run1 = paragraph.add_run(f"{date} ({weekday(date)})")
-            run1.style = 'Heading 3 Char'
-            run = paragraph.add_run(':')
-
-
-
+        if paragraph.runs:
+            # Enhance date patterns in the inline text
+            for i in range(len(paragraph.runs)):
+                paragraph.runs[i].text = enhance_date_with_weekday(paragraph.runs[i].text)
 
 main()
 
 # Save the modified document
 document.save(changed_document_path)
+
